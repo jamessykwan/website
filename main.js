f78cbd3ecb905f565a9508a777aac67ae4b6e134
jamessykwan <jamessykwan@gmail.com>
2019-02-22 20:39:33 -0800

    added text box

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-let paragraph = document.getElementById('text-area');
+$(document).ready(function () {
+    $("#text_box").each(function () {
+        var text = $(this);
+
+        // Save current value of the text box
+        text.data("oldVal", text.val());
+
+        // Look for changes in value
+        text.on("propertychange", function (event) {
+            if (text.data("oldVal") != text.val) {
+                text.val = "hi";
+                text.data("oldVal", text.val());
+            }
+
+        })
+    })
+});
